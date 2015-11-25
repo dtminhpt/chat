c8e7dedd22ac6128aaca98f82202b983245d6a04
Minh Dinh <dtminhpt@gmail.com>
2015-11-25 19:58:43 +0700

    Initial Commit

---
+++ chat/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  chat
+//
+//  Created by Dinh Thi Minh on 11/25/15.
+//  Copyright © 2015 Dinh Thi Minh. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
