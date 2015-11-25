9b9ad26b2bb587a6ad9f422ba2d9e2b298450120
Ngo Thanh Tai <ngothanhtai89@yahoo.com>
2015-11-25 20:25:53 +0700

    Milestone 2 Signup/Login

--- chat/ViewController.swift
+++ chat/ViewController.swift
@@ -7,9 +7,14 @@
 //
 
 import UIKit
+import Parse
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var usernameTextField: UITextField!
+    @IBOutlet weak var passwordTextField: UITextField!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +25,49 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func onLogin(sender: AnyObject) {
+        
+        self.login(usernameTextField.text!, password: passwordTextField.text!)
+    }
+    
+    @IBAction func onSignup(sender: AnyObject) {
+        self.signup(self.usernameTextField.text!, password: passwordTextField.text!)
+    }
 
+    func login(username:String, password:String) {
+        PFUser.logInWithUsernameInBackground(username, password:password) {
+            (user: PFUser?, error: NSError?) -> Void in
+            if user != nil {
+                // Do stuff after successful login.
+            } else {
+                print(error!)
+                // The login failed. Check error to see why.
+                let errorString = error!.userInfo["error"] as? NSString
+                let alertView = UIAlertView(title: "Error message", message: errorString! as String, delegate: nil, cancelButtonTitle: "Close")
+                alertView.show()
+            }
+        }
+    }
+    
+    func signup(username:String, password:String) {
+        var user = PFUser()
+        user.username = username
+        user.password = password
+        user.email = "email@example.com"
+        // other fields can be set just like with PFObject
+        user["phone"] = "415-392-0202"
+        
+        user.signUpInBackgroundWithBlock {
+            (succeeded: Bool, error: NSError?) -> Void in
+            if let error = error {
+                let errorString = error.userInfo["error"] as? NSString
+                let alertView = UIAlertView(title: "Error message", message: errorString! as String, delegate: nil, cancelButtonTitle: "Close")
+                alertView.show()
+                // Show the errorString somewhere and let the user try again.
+            } else {
+                // Hooray! Let them use the app now.
+            }
+        }
+    }
 }
 
